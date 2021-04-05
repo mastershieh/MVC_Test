bf75868902a7b2efa541a04274ac841dfa9d5b1c
mastershieh <xie.zuren2010@gmail.com>
2021-04-05 15:59:44 +0800

    加入專案檔案。

---
+++ MVC_Test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MVC_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+ 
+
+        public ActionResult Index()
+        {
+            DateTime date = DateTime.Now;
+            Student data = new Student();
+            List<Student> list = new List<Student>();
+            list.Add(new Student("1", "小明", 80));
+            list.Add(new Student("2", "小華", 70));
+            list.Add(new Student("3", "小英", 60));
+            list.Add(new Student("4", "小李", 50));
+            list.Add(new Student("5", "小張", 40));
+
+            ViewBag.Date = date;
+            ViewBag.Student = data;
+            ViewBag.List = list;
+
+            return View();
+        }
+        public ActionResult Hello()
+        {
+
+            ViewData["Hello"] = "Hello World ";
+            ViewBag.MVC = "Hello MVC";
+
+
+            return View();
+
+        }
+
+
+    }
+}
---
+++ MVC_Test/Models/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Test.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+
+        public Student()
+        {
+            id = string.Empty;
+            name = string.Empty;
+            score = 0;
+        }
+        public Student(string _id, string _name, int _score)
+        {
+            id = _id;
+            name = _name;
+            score = _score;
+        }
+        public override string ToString()
+        {
+            return $"學號:{id},姓名:{name},分數:{score}.";
+        }
+    }
+}
